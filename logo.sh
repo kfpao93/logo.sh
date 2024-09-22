9168227a106becee3bcb63d61a015ed93fccee24
kfpao93 <55264108+kfpao93@users.noreply.github.com>
2024-09-22 22:34:13 +0700

    Update logo.sh

--- logo.sh
+++ logo.sh
@@ -11,7 +11,7 @@ echo -e "\e[1;32m
 
 echo -e "\e[1;31m
 ◤━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◥
-    Telegram: https://t.me/rich
+    Telegram: https://t.me/richjoker
    
 ◣━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━◢
 
